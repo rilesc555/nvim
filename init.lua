761f1ce0b4206914f1aee97f0da9490ee5ccfb0d
rilesc555 <rilesconnors555@gmail.com>
2025-01-29 12:28:30 -0600

    start adding harpoon

--- init.lua
+++ init.lua
@@ -377,7 +377,46 @@ require('lazy').setup({
       },
     },
   },
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local harpoon = require 'harpoon'
+      harpoon:setup()
 
+      vim.keymap.set('n', '<leader>ha', function()
+        harpoon:list():prepend()
+      end, { desc = '[H]arpoon add to list' })
+      vim.keymap.set('n', '<C-e>', function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end)
+      vim.keymap.set('n', '<C-h>', function()
+        harpoon:list():select(1)
+      end)
+      vim.keymap.set('n', '<C-j>', function()
+        harpoon:list():select(2)
+      end)
+      vim.keymap.set('n', '<C-k>', function()
+        harpoon:list():select(3)
+      end)
+      vim.keymap.set('n', '<C-l>', function()
+        harpoon:list():select(4)
+      end)
+      vim.keymap.set('n', '<leader><C-h>', function()
+        harpoon:list():replace_at(1)
+      end)
+      vim.keymap.set('n', '<leader><C-j>', function()
+        harpoon:list():replace_at(2)
+      end)
+      vim.keymap.set('n', '<leader><C-k>', function()
+        harpoon:list():replace_at(3)
+      end)
+      vim.keymap.set('n', '<leader><C-l>', function()
+        harpoon:list():replace_at(4)
+      end)
+    end,
+  },
   -- NOTE: Plugins can specify dependencies.
   --
   -- The dependencies are proper plugin specifications as well - anything
@@ -458,6 +497,7 @@ require('lazy').setup({
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
       pcall(require('telescope').load_extension, 'file-browser')
+      pcall(require('telescope').load_extension, 'harpoon')
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
