fbddc7fbf374a1a5f51618d83c8d2e9f173dd2c3
Riley Connors <rilesconnors@gmail.com>
2025-07-16 12:53:29 -0500

    feat: remove copilot

--- init.lua
+++ init.lua
@@ -223,7 +223,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 vim.keymap.set('n', '<leader>tw', toggle_warnings, { desc = '[T]oggle warnings' })
 
-
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
