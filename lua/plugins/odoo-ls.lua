152d4cff76161858b13f5d4fd66f8b9a805a5d11
rilesc555 <rilesconnors@gmail.com>
2025-07-17 14:21:44 -0500

    chore: Refine odoo-ls configuration paths and options

--- lua/plugins/odoo-ls.lua
+++ lua/plugins/odoo-ls.lua
@@ -2,25 +2,20 @@ return {
   'Whenrow/odoo-ls.nvim',
   dependencies = { 'neovim/nvim-lspconfig' },
   config = function()
-    local odools = require('odools')
-    local h = os.getenv('HOME')
-    odools.setup({
+    local odools = require 'odools'
+    local h = os.getenv 'HOME'
+    odools.setup {
       -- mandatory
-      odoo_path = h .. '/Dev/odoo/src/odoo/',
-      -- Find your python path by running `which python3` in your terminal.
-      -- It should point to the python executable used for your Odoo project.
-      python_path = '/usr/bin/python3', -- NOTE: Please update this path
-      server_path = h .. '/.local/bin/odoo_ls_server',
+      odoo_path = h .. '/dev/odoo/odoo/',
+      python_path = h .. '/.env/bin/python',
 
       -- optional
-      addons = { h .. '/Dev/odoo/src/odoo/addons', h .. '/Dev/odoo/src/enterprise/', h .. '/Dev/odoo/src/user/' },
-      additional_stubs = { h .. '/Dev/odoo/src/typeshed/stubs' },
-      root_dir = h .. '/Dev/odoo/src/', -- working directory, odoo_path if empty
+      addons = { h .. '/dev/odoo/odoo/addons' },
       settings = {
         autoRefresh = true,
         autoRefreshDelay = nil,
         diagMissingImportLevel = 'none',
       },
-    })
+    }
   end,
 }
