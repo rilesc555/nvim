aa2ce714ebfa5b52d2b863107a58b17800e1befe
rilesc555 <rilesconnors@gmail.com>
2025-07-24 17:09:00 -0500

    eh

--- lua/plugins/odoo-ls.lua
+++ lua/plugins/odoo-ls.lua
@@ -9,19 +9,19 @@ return {
       -- NOTE: Update these paths to match your system
       odoo_path = h .. '/dev_wsl/odoo/odoo/odoo',
       python_path = '/usr/bin/python3',
-      addons_paths = { h .. '/dev_wsl/odoo/enterprise/odoo/addons', h .. '/dev_wsl/odoo/vertec_addons' },
-      -- additional_stubs = { h .. '/dev/odoo/additional_stubs' },
-
-      -- Auto-install the language server if not found
-      auto_install = true,
-
-      -- LSP settings
-      settings = {
-        Odoo = {
-          autoRefresh = true,
-          diagMissingImportLevel = 'none',
-        },
-      },
+      -- addons_paths = { h .. '/dev_wsl/odoo/enterprise/odoo/addons', h .. '/dev_wsl/odoo/vertec_addons' },
+      -- -- additional_stubs = { h .. '/dev/odoo/additional_stubs' },
+      --
+      -- -- Auto-install the language server if not found
+      -- auto_install = true,
+      --
+      -- -- LSP settings
+      -- settings = {
+      --   Odoo = {
+      --     autoRefresh = true,
+      --     diagMissingImportLevel = 'none',
+      --   },
+      -- },
     }
   end,
 }
